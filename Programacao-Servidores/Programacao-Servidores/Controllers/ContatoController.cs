a94196f10390e1d75a94748144f0e548e5d92003
Guilherme <paixaoguilherme79@gmail.com>
2026-08-17 20:45:52 -0300

    feat: TP01

---
+++ Programacao-Servidores/Programacao-Servidores/Controllers/ContatoController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Programacao_Servidores.Models;
+
+namespace Programacao_Servidores.Controllers
+{
+    public class ContatoController : Controller
+    {
+        public IActionResult Index()
+        {
+            ListaContatosModel modelo = new ListaContatosModel();
+            return View(modelo);
+        }
+    }
+}
---
+++ Programacao-Servidores/Programacao-Servidores/Models/ContatoModel.cs
@@ -0,0 +1,29 @@
+﻿namespace Programacao_Servidores.Models
+{
+    public class ContatoModel
+    {
+        public long Id { get; set; }
+        public string Nome { get; set; }
+        public string Celular { get; set; }
+        public string Email { get; set; }
+
+        private static long contator = 0;
+
+        public ContatoModel(String Nome, String Celular, String Email)
+        {
+            this.Nome = Nome;
+            this.Celular = Celular;
+            this.Email = Email;
+            Id = contator++;
+        }
+    }
+    public class ListaContatosModel
+    {
+        public List<ContatoModel> Lista = new List<ContatoModel>
+        {
+            new ContatoModel("Maria", "9 9876 6754", "maria@yahoo.com"),
+            new ContatoModel("Carlos", "9 9876 9950", "carlos@yahoo.com"),
+            new ContatoModel("João", "9 9123 4567", "joao@gmail.com")
+        };
+    }
+}
