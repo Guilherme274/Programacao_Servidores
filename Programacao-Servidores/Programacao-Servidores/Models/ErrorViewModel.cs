c62a39c512b40546948cc7bcf6e77aad41db736f
unisanta <unisanta@santacecilia.br>
2026-08-17 19:56:48 -0300

    Initial commit

---
+++ Programacao-Servidores/Programacao-Servidores/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Programacao_Servidores.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
