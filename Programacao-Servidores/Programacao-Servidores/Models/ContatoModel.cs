e63fbb2950331faf817aed48b5314fb249a963cb
Guilherme <paixaoguilherme79@gmail.com>
2026-08-17 21:19:35 -0300

    ContatoMVC com lista alfabetica e ID

--- Programacao-Servidores/Programacao-Servidores/Models/ContatoModel.cs
+++ Programacao-Servidores/Programacao-Servidores/Models/ContatoModel.cs
@@ -14,7 +14,7 @@
             this.Nome = Nome;
             this.Celular = Celular;
             this.Email = Email;
-            Id = contator++;
+            this.Id = contator++;
         }
     }
     public class ListaContatosModel
@@ -22,8 +22,11 @@
         public List<ContatoModel> Lista = new List<ContatoModel>
         {
             new ContatoModel("Maria", "9 9876 6754", "maria@yahoo.com"),
+            new ContatoModel("Enzo", "9 9123 7643", "enzo@tropical.com"),
             new ContatoModel("Carlos", "9 9876 9950", "carlos@yahoo.com"),
-            new ContatoModel("João", "9 9123 4567", "joao@gmail.com")
+            new ContatoModel("Guigalherme", "9 1423 0143", "guigalherme@fortnite.com"),
+            new ContatoModel("João", "9 9123 4567", "joao@gmail.com"),
+            new ContatoModel("Léo", "9 9821 7223", "léo@gmail.com")
         };
     }
 }
